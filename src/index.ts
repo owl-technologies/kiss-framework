64f19feb438c94c5783441a69c0d2b4d533f4483
Sanerix <eric.faddeev@gmail.com>
2024-03-27 10:38:00 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,15 @@ export { Optional } from "./kiss-data/decorators/optional.js";
 export { RejectAfter } from "./kiss-data/decorators/reject-after.js";
 export { Required } from "./kiss-data/decorators/required.js";
 export { Constructor, ConstructorOrFunction, Delta, GatewayFn, OpLink, SearchableById, SearchableByName } from "./kiss-data/decorators/types.js";
-export { CidLink } from "./kiss-data/initializers/cid-link.js";
-export { GetSetCid, GetSetCidArray, InitCid, InitCidArray } from "./kiss-data/initializers/init-cid.js";
 export { GetSetJson, GetSetJsonArray, InitJson, InitJsonArray } from "./kiss-data/initializers/init-json.js";
-export { GetSetById, GetSetByIdArray, GetSetByName, GetSetByNameArray, InitById, InitByIdArray, InitByName, InitByNameArray } from "./kiss-data/initializers/init-server-link.js";
-export { OpServerLink } from "./kiss-data/initializers/server-link.js";
-export { CURRENT_VERSION, FIELD_METADATA, KissData, RegisterMigrate, CURRENT_VERSION as version } from "./kiss-data/kiss-data.js";
+export { FIELD_METADATA, KissData, RegisterMigrate } from "./kiss-data/kiss-data.js";
 export { Controller, KissServer } from "./kiss-web/Controller.js";
 export { Get, Post } from "./kiss-web/RequestTypes.js";
 export { Service } from "./kiss-web/Service.js";
 export { assert } from "./utils/assert.js";
-export { base } from "./utils/base-x.js";
-export { calculateCID } from "./utils/calculate-cid.js";
-export { isCid } from "./utils/is-cid.js";
 export { isClass } from "./utils/is-class.js";
 export { isConstructor } from "./utils/is-constructor.js";
 export { describe, expect, it, test } from "./utils/not-jest.js";
 export { metadata } from "./utils/reflect-metadata.js";
-export { tictoc } from "./utils/tic-toc.js";
+
 
