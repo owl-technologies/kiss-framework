ce837b6c04e98511a70c8d66dee737223d4284d1
Sanerix <eric.faddeev@gmail.com>
2024-04-17 16:50:22 +0200

    fixed allKeys function to prevent access to private storage

--- src/utils/allKeys.ts
+++ src/utils/allKeys.ts
@@ -3,9 +3,15 @@ export function allKeys(obj) {
     while (obj = Reflect.getPrototypeOf(obj)) {
         let keys = Reflect.ownKeys(obj)
         keys.forEach((k) => {
-            if (typeof obj[k] === 'function') {
-                methods.push(k);
+            // Skip private properties
+            if (typeof k === 'string' && k.startsWith('#')) {
+                return;
             }
+            let descriptor = Object.getOwnPropertyDescriptor(obj, k);
+            // for accessors the descriptor is {get: ƒ, set: ƒ, enumerable: false, configurable: true}
+            if (descriptor && (descriptor.get || descriptor.set) && descriptor.enumerable) {
+                methods.push(k);
+            } 
         });
     }
     return methods;
