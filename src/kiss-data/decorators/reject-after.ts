c6d5331135c45eb9c99e1988cbcbb867386dd07b
Sanerix <eric.faddeev@gmail.com>
2024-07-02 16:26:31 +0200

    RejectAfter return to original

--- src/kiss-data/decorators/reject-after.ts
+++ src/kiss-data/decorators/reject-after.ts
@@ -1,30 +1,32 @@
-export function RejectAfter(timeout: number, message: string) {
-  return function ( method, context: ClassMethodDecoratorContext) {
-      return function (this, ...args) {
-          return new Promise((resolve, reject) => {
-              const timer = setTimeout(() => {
-                  reject(new Error(message));
-              }, timeout);
-
-              const promise = method.apply(this, args);
-
-              Promise.resolve(promise)
-                  .then(result => {
-                      clearTimeout(timer);
-                      resolve(result);
-                  })
-                  .catch(error => {
-                      clearTimeout(timer);
-                      reject(error);
-                  });
-          })
-      }
+export function RejectAfter(timeout, message) {
+    return function <T, V extends (...args: any) => Promise<any>,
+    >(method: V, context: ClassMethodDecoratorContext<T, V>) {
+      return function (this: T, ...args: any) {
+        return new Promise((resolve, reject) => {
+          const timer = setTimeout(() => {
+            reject(new Error(message));
+          }, timeout);
+  
+          const promise = method.apply(this, args);
+  
+          Promise.resolve(promise)
+            .then(result => {
+              clearTimeout(timer);
+              resolve(result);
+            })
+            .catch(error => {
+              clearTimeout(timer);
+              reject(error);
+            });
+        })
+      } as V;
+    }
   }
-}
+  
 
 // class Test {
 //   @RejectAfter(1000, 'Timeout')
 //   async test() {
-//       await new Promise(resolve => setTimeout(resolve, 2000));
+//       return await new Promise(resolve => setTimeout(resolve, 2000));
 //   }
 // }
