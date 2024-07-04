ea4946750bc05cf2d44133721b56a0b221132ac3
Sanerix <eric.faddeev@gmail.com>
2024-07-04 17:27:57 +0200

    extra coma

--- src/kiss-data/decorators/reject-after.ts
+++ src/kiss-data/decorators/reject-after.ts
@@ -1,6 +1,5 @@
 export function RejectAfter(timeout, message) {
-    return function <T, V extends (...args: any) => Promise<any>,
-    >(method: V, context: ClassMethodDecoratorContext<T, V>) {
+    return function <T, V extends (...args: any) => Promise<any>>(method: V, context: ClassMethodDecoratorContext<T, V>) {
       return function (this: T, ...args: any) {
         return new Promise((resolve, reject) => {
           const timer = setTimeout(() => {
